83645b66f07931b409f6d80de5c0a6e17f421990
gguzhov <g.guzhov77@gmail.com>
2022-10-19 18:37:53 +0300

    Delete not needed

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
-<<<<<<<<< Temporary merge branch 1
 a = 'Диана любит Марго'
 print(a)
 b = 'Наш проект по телеграмм боту'
 print(b)
-=========
-~/code mkdir google-sheets
-~/ code cd google-sheets
-~/ 
->>>>>>>>> Temporary merge branch 2
